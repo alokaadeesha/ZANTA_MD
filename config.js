3ebea81bee6649838fd368d63d57b48f68d72302
Akashkavindu <143059042+Akashkavindu@users.noreply.github.com>
2025-12-09 04:27:52 -0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ return text === fault ? true : false;
 }
 
 
-const OWNER_NUMBER = '94743404814'; // ඔබගේ Bot Owner ගේ අංකය
+const OWNER_NUMBER = '94766247995'; // ඔබගේ Bot Owner ගේ අංකය
 const DEFAULT_BOT_NAME = process.env.BOT_NAME || "ZANTA-MD";
 const ALIVE_MSG_TEMPLATE = aliveMessageData.getAliveMessage();
 
@@ -18,7 +18,7 @@ const FINAL_ALIVE_MSG = ALIVE_MSG_TEMPLATE
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "8MsmwD4K#912RVQxK8vQZHN3adk6Di2tO1w6xQaYkmWtM4lTw97k", //Your session id
+SESSION_ID: process.env.SESSION_ID || "5MlkWDTY#pqLKwj9f4TIQv4g2gDbvVTxaLWCFzAxvGxKhJwIrmQ8", //Your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Akashkavindu/ZANTA_MD/refs/heads/main/images/ChatGPT%20Image%20Nov%2021%2C%202025%2C%2001_21_32%20AM.png",
 ALIVE_MSG: process.env.ALIVE_MSG || FINAL_ALIVE_MSG, 
 BOT_OWNER: OWNER_NUMBER, 
